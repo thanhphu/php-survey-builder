1114fe5d14dd386560d041b4575e4b8aab8eca67
Thanh Phu <git@thanhphu.net>
2020-05-01 16:33:53 +0900

    Survey form materialized

--- views/footer.php
+++ views/footer.php
@@ -1,2 +1,12 @@
-<div id="footer">
-</div>
+  <footer class="mdl-mega-footer">
+    <div class="mdl-mega-footer--bottom-section">
+      <div class="mdl-logo">
+        More Information
+      </div>
+      <ul class="mdl-mega-footer--link-list">
+        <li><a href="https://developers.google.com/web/starter-kit/">Web Starter Kit</a></li>
+        <li><a href="#">Help</a></li>
+        <li><a href="#">Privacy and Terms</a></li>
+      </ul>
+    </div>
+  </footer>
