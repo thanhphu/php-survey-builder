2fc734970aa7186040fa1a6e9f177d0cee077197
Thanh Phu <git@thanhphu.net>
2020-05-16 22:03:55 +0900

    Branding

--- views/stylesheets-material.php
+++ views/stylesheets-material.php
@@ -1,8 +1,8 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="description" content="A front-end template that helps you build fast, modern mobile web apps.">
+  <meta name="description" content="The world's opinion, in your hand. 사장님의 힘!">
   <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
-  <title>NetViet survey app</title>
+  <title>NetViet survey app | 넷비엣 조사앱</title>
 
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&amp;lang=en">
   <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
